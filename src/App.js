38e8f4913d1f9bbd5978bea6ee2fb49cdeae451b
Diego <dam_900@hotmail.com>
2019-02-17 11:01:47 -0600

    bleach

--- src/App.js
+++ src/App.js
@@ -14,8 +14,10 @@ class App extends Component {
         <BrowserRouter>
           <div>
             <Cabeza />
-            <div>
-              <p>aqui ira el home</p>
+            <div class="your-class">
+              <div><img src="https://cdn.pixabay.com/photo/2015/07/02/10/40/writing-828911_960_720.jpg" alt=""></img></div>
+              <div><img src="http://dailypost.ng/wp-content/uploads/2016/02/pencils.jpg" alt=""></img></div>
+              <div><img src="https://partysupplyfactory.com/wp-content/uploads/2017/04/81vygUugWRL._SL1500_.jpg" alt=""></img></div>
             </div>
             <Switch>
               <Route path="/" exact />
