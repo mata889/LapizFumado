52ef989f122e8de26ab373643083b7abf0f92e4f
Diego <dam_900@hotmail.com>
2019-02-17 11:18:59 -0600

    vamos

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import Cabeza from "./header";
 import Carrito from "./Paginas/Carrito";
-
+import Style from "./slick/slick.css"
 import Productos from "./Paginas/Productos";
 
 
 class App extends Component {
+
   render() {
+
     return (
+
       <div id="cabeza">
+        <link rel="stylesheet" type="text/css" href="slick/slick.css" />
+
+        <link rel="stylesheet" type="text/css" href="slick/slick-theme.css" />
         <BrowserRouter>
           <div>
             <Cabeza />
@@ -22,11 +28,22 @@ class App extends Component {
             <Switch>
               <Route path="/" exact />
               <Route path="/Productos" component={Productos} exact />
-              
+
             </Switch>
           </div>
         </BrowserRouter>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="slick/slick.min.js"></script>
+        <script type="text/javascrip">
+        $(document).ready(function(){
+        $('.your-class').slick(
+        )
+        });
+		
+        </script>
       </div>
+
     )
   }
 }
